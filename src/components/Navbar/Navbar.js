2ff2e95153a856e23495ef85793b41f5e3dd6a15
daudi13 <davouma13@gmail.com>
2022-03-24 17:24:59 +0300

    Add Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  <div className="navBar">
+    <div className="logo">BookStore CMS</div>
+    <ul className="nav">
+      <li>Books</li>
+      <li>Categories</li>
+    </ul>
+    <div className="userIcon">A</div>
+  </div>;
+};
+
+export default Navbar;
