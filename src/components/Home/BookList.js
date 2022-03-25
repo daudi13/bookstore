b2d80dc3b22e818b874869fc7931053964d2c008
daudi13 <davouma13@gmail.com>
2022-03-25 11:48:02 +0300

    change page title

--- src/components/Home/BookList.js
+++ src/components/Home/BookList.js
@@ -4,7 +4,7 @@ import './BookList.css';
 
 const BookList = () => (
   <ul className="booklist-box">
-    <Book title="In the Heart of the Sea" author="Mark Twain" genre="Fantacy" complete="55%" chapter="12" />
+    <Book title="In the Heart" author="Mark Twain" genre="Fantacy" complete="55%" chapter="12" />
     <Book title="Hunger Games" author="Suzzanne Collins" genre="Action" complete="93%" chapter="33" />
     <Book title="Bourne Legacy" author="Robert Lundlum" genre="Suspense" complete="3%" chapter="27" />
   </ul>
