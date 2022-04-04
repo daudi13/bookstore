72ff37555abd5fb87f7ad8ef63a7cd879d7ff07c
daudi13 <davouma13@gmail.com>
2022-04-04 16:32:13 +0300

    Add initial state

--- src/components/Home/BookList.js
+++ src/components/Home/BookList.js
@@ -2,12 +2,54 @@ import React from 'react';
 import Book from './Book/Book';
 import './BookList.css';
 
-const BookList = () => (
-  <ul className="booklist-box">
-    <Book title="In the Heart" author="Mark Twain" genre="Fantacy" complete="55%" chapter="12" />
-    <Book title="Hunger Games" author="Suzzanne Collins" genre="Action" complete="93%" chapter="33" />
-    <Book title="Bourne Legacy" author="Robert Lundlum" genre="Suspense" complete="3%" chapter="27" />
-  </ul>
-);
+const BookList = () => {
+  const [books, setBook] = React.useState([
+    {
+      id: 1,
+      author: 'Mark Twain',
+      title: 'In the heart',
+      complete: '55%',
+      chapter: '12',
+      genre: 'fiction',
+    },
+    {
+      id: 2,
+      author: 'Mark Twain',
+      title: 'In the heart',
+      complete: '55%',
+      chapter: '12',
+      genre: 'fiction',
+    },
+    {
+      id: 3,
+      author: 'Mark Twain',
+      title: 'In the heart',
+      complete: '55%',
+      chapter: '12',
+    }]);
+
+  const createBook = () => (
+    setBook((prevBook) => prevBook)
+  );
+
+  return (
+    <ul className="booklist-box">
+      {
+        books.map((book) => (
+          <Book
+            func={createBook}
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            genre={book.genre}
+            complete={book.complete}
+            chapter={book.chapter}
+          />
+        ))
+    }
+
+    </ul>
+  );
+};
 
 export default BookList;
