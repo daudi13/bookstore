c968341caa8b3e3a69a35953466440c8c482ceef
daudi13 <davouma13@gmail.com>
2022-04-05 18:21:28 +0300

    link thunk to redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import categoriesReducer from './categories/categories';
 import reducer from './books/books';
 
@@ -9,5 +10,5 @@ const root = combineReducers(
   },
 );
 
-const store = createStore(root);
+const store = createStore(root, applyMiddleware(thunk));
 export default store;
