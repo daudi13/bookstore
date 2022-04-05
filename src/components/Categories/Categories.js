c01e036f06468184f27c1c71089af79743f6d635
daudi13 <davouma13@gmail.com>
2022-04-05 13:16:48 +0300

    Add usedispatch on categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { categoriesStatus } from '../../redux/categories/categories';
 
-const Categories = () => (
-  <>
-    <button type="button">Check Status</button>
-  </>
-);
+const Categories = () => {
+  const Categories = useSelector((state) => state.categoriesReducer);
+  const dispatch = useDispatch();
+  const handleAction = () => (
+    dispatch(categoriesStatus())
+  );
+
+  return (
+    <>
+      <button type="button" onClick={handleAction}>Check Status</button>
+      <br />
+      <br />
+      <br />
+      {Categories}
+    </>
+  );
+};
 
 export default Categories;
