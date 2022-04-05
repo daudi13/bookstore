045d5939aaa86551d66e6a16fa152b14d7c50840
daudi13 <davouma13@gmail.com>
2022-04-05 12:18:58 +0300

    Add dispatch to add button

--- src/components/Home/BookForm.js
+++ src/components/Home/BookForm.js
@@ -1,28 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import uuid from 'react-uuid';
+import { addbook } from '../../redux/books/books';
 import './BookForm.css';
 
-const BookForm = () => (
-  <form>
-    <h3>Add New Books</h3>
-    <div className="form-container">
-      <label htmlFor="book-title">
-        <input type="text" htmlFor="book-title" placeholder="book title" className="input" />
-      </label>
-      <label htmlFor="book-author">
-        <input type="text" htmlFor="book-book-author" placeholder="book authour" className="input" />
-      </label>
-      <label htmlFor="categories">
-        <select id="books categories" className="category">
-          <option>Categories</option>
-          <option>Fiction</option>
-          <option>Non-fiction</option>
-          <option>Self-Help</option>
-          <option>financial</option>
-        </select>
-      </label>
-      <button type="submit">Add Book</button>
-    </div>
-  </form>
-);
+const BookForm = () => {
+  const [book, setBook] = useState({
+    id: '',
+    title: '',
+    author: '',
+  });
+
+  console.log(book);
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    return setBook((prevbook) => ({
+      ...prevbook,
+      id: uuid(),
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addbook(book));
+  };
+
+  return (
+    <form>
+      <h3>Add New Books</h3>
+      <div className="form-container">
+        <label htmlFor="book-title">
+          <input
+            type="text"
+            htmlFor="book-title"
+            placeholder="book title"
+            className="input"
+            name="title"
+            value={book.title}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="book-author">
+          <input
+            type="text"
+            htmlFor="book-book-author"
+            placeholder="book authour"
+            className="input"
+            name="author"
+            value={book.author}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="categories">
+          <select id="books categories" className="category">
+            <option>Categories</option>
+            <option>Fiction</option>
+            <option>Non-fiction</option>
+            <option>Self-Help</option>
+            <option>financial</option>
+          </select>
+        </label>
+        <button type="submit" onClick={handleSubmit}>Add Book</button>
+      </div>
+    </form>
+  );
+};
 
 export default BookForm;
