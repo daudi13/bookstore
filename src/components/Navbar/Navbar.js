1acca3e168d4b2f278530912647b6235530b9b28
daudi13 <davouma13@gmail.com>
2022-04-07 16:03:42 +0300

    Add styling to nav buttons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
@@ -8,9 +8,9 @@ const Navbar = () => (
       <nav>
         <div className="logo">BookStore &reg;</div>
         <ul className="nav">
-          <li><Link to="/">Home</Link></li>
+          <li><NavLink className="link-btn" to="/">Home</NavLink></li>
           <hr />
-          <li><Link to="Categories">Category</Link></li>
+          <li><NavLink className="link-btn" to="Categories">Category</NavLink></li>
         </ul>
         <div className="userIcon">.</div>
       </nav>
