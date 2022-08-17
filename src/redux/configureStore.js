82fc9f79968d1c23b05fce618d484cfbebe2d52a
daudi13 <davouma13@gmail.com>
2022-08-17 21:10:57 +0300

    fix errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     user: userReducer,
   },
 });
+
+export default store;
