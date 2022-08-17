fc2fda12242cd1212bc55031f6210b1655b7f271
daudi13 <davouma13@gmail.com>
2022-08-17 15:49:09 +0300

    add userslice logic

--- src/redux/user/users.js
+++ src/redux/user/users.js
@@ -1,47 +1,25 @@
-import LoginDatabase from '../../../database/LoginUser';
+import { createSlice } from '@reduxjs/toolkit'
 
-const ADD_LOGIN_REQUEST = 'bookstore/login/GET_LOGIN_REQUEST';
-const ADD_LOGIN_SUCCESS = 'bookstore/login/GET_LOGIN_SUCCESS';
-const ADD_LOGIN_FAIL = 'bookstore/login/GET_LOGIN_FAIL';
-
-const GET_LOGIN_REQUEST = 'bookstore/login/GET_LOGIN_REQUEST';
-const GET_LOGIN_SUCCESS = 'bookstore/login/GET_LOGIN_SUCCESS';
-const GET_LOGIN_FAIL = 'bookstore/login/GET_LOGIN_FAIL';
-
-const intialState = {
-  loading: false,
-  error: '',
-  user: {},
-};
-
-export const addLoginRequest = () => ({
-  type: ADD_LOGIN_REQUEST,
-});
-
-export const addLoginSuccess = (user) => ({
-  type: ADD_LOGIN_SUCCESS,
-  payload: user,
-});
-
-export const addLoginFail = (error) => ({
-  type: ADD_LOGIN_FAIL,
-  payload: error,
-});
-
-export function addLogin(user) {
-  return (dispatch) => {
-    dispatch(addLoginRequest());
-    const {
-      email, password,
-    } = user;
-    LoginDatabase.loginUser(email, password)
-      .then(() => {
-        const userNew = {};
-        userNew[email] = [{ email, password }];
-        dispatch(addLoginSuccess(userNew));
-      })
-      .catch((error) => {
-        dispatch(addLoginFail(error.message));
-      });
-  };
+const initialState = {
+  loggedIn: false,
+  user: null
 }
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    logUserin: (state, action) => {
+      state.loggedIn = true;
+      state.user = action.payload;
+    },
+    logUserOut: (state) => {
+      state.loggedIn = false
+      state.user = null;
+    },
+  },
+});
+
+export const { logUserin, logUserOut } = userSlice.actions
+
+export default userSlice.reducer;
