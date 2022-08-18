a88f7b45f0596da39c8db377e3cb199cc537e504
daudi13 <davouma13@gmail.com>
2022-08-18 11:29:47 +0300

    configure persist redux to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import userReducer from './user/userSlice';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
+const persistConfig = {
+  key: 'counter',
+  storage,
+};
 
-export default store;
+const reducers = combineReducers({ user: userReducer });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
