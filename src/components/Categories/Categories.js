ec47d7eadb9e5d9493dfaae1faa42d6a984a8b02
daudi13 <davouma13@gmail.com>
2022-08-18 10:39:14 +0300

    install redux-persist

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -4,11 +4,12 @@ import { categoriesStatus } from '../../redux/categories/categories';
 
 const Categories = () => {
   const Categories = useSelector((state) => state.categoriesReducer);
+  const user = useSelector((state) => state.user)
   const dispatch = useDispatch();
   const handleAction = () => (
     dispatch(categoriesStatus())
   );
-
+  console.log(user)
   return (
     <>
       <button type="button" className="btn-category" onClick={handleAction}>Check Status</button>
