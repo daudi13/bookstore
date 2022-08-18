08a24bbf289feb9e8ef10182e638ef29f090c116
daudi13 <davouma13@gmail.com>
2022-08-18 11:31:43 +0300

    add persist redux to global state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import Navbar from './components/Navbar/Navbar';
 import reportWebVitals from './reportWebVitals';
@@ -11,18 +13,22 @@ import store from './redux/configureStore';
 import LoginPage from './components/LoginPage/LoginPage';
 import SignUpPage from './components/SignUpPage/SignUpPage';
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/Categories" element={<Categories />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="sign-up" element={<SignUpPage />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/Categories" element={<Categories />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="sign-up" element={<SignUpPage />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
