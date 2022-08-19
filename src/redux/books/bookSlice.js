fed3dd65d5c99b1a87aae75fbae8c84e363e145d
daudi13 <davouma13@gmail.com>
2022-08-19 20:47:40 +0300

    add object destructure in postBook function

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -5,24 +5,25 @@ const initialState = {
   isLoading: false,
 };
 
-export const postBook = createAsyncThunk('books/postbooks', async (body, userId) => {
-  await fetch(`http://localhost:3001/users/${userId}/books`, {
+export const postBook = createAsyncThunk('books/postbooks', async ({data, id}) => {
+  await fetch(`http://localhost:3001/users/${id}/books`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       book: {
-        title: body.title,
-        author: body.author,
-        genre: body.genre,
-        userId,
-        current_chapter: body.current_chapter,
-        total_chapters: body.total_chapters,
+        title: data.title,
+        author: data.author,
+        genre: data.genre,
+        user_id: id,
+        current_chapter: data.current_chapter,
+        total_chapters: data.total_chapters,
       },
     }),
   });
 });
+
 
 export const getBooks = createAsyncThunk('books/getBooks', async (userId) => {
   const res = await fetch(`http://localhost:3001/users/${userId}/books`);
