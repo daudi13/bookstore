6017bbf9f23e1c879bf823159f340532abdb8c91
daudi13 <davouma13@gmail.com>
2022-08-19 20:52:14 +0300

    temporarily remove bookList component from BookContainer

--- src/components/Home/BookContainer.js
+++ src/components/Home/BookContainer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import BookForm from './BookForm';
-import BookList from './BookList';
 import './BookContainer.css';
 
 const BookContainer = () => (
   <div className="row">
-    <BookList />
     <BookForm />
   </div>
 );
