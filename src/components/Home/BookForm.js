61667fa1aff507b1ff13f9405f6ab897fd1225f0
daudi13 <davouma13@gmail.com>
2022-08-19 20:50:39 +0300

    include id and data in object

--- src/components/Home/BookForm.js
+++ src/components/Home/BookForm.js
@@ -1,36 +1,35 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { current } from '@reduxjs/toolkit';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { postBook } from '../../redux/books/bookSlice';
 import './BookForm.css';
 
 const BookForm = () => {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   dispatch(postBook(book));
-  //   document.querySelector('.one').value = '';
-  //   document.querySelector('.two').value = '';
-  // };
+  const currentUser = useSelector((state) => state.user.user.user);
+  const { id } = currentUser;
 
-  const onSubmit = async (data, userId) => {
-    dispatch(postBook(data, userId));
-    document.querySelector('.one').value = '';
-    document.querySelector('.two').value = '';
+  
+  const onSubmit = async (data) => {
+    const meta = {id, data};
+    dispatch(postBook(meta));
+    document.querySelectorAll('input').value = '';
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
       <h3>Add New Books</h3>
       <div className="form-container">
         <label htmlFor="book-title">
           <input
             type="text"
             htmlFor="book-title"
-            className="input one"
-            {...register('title', { required: true, placeholder: 'Add book title', minLength: 2 })}
+            placeholder="book title"
+            className="input"
+            {...register('title', { required: true, minLength: 2 })}
           />
         </label>
         <label htmlFor="book-author">
@@ -38,7 +37,7 @@ const BookForm = () => {
             type="text"
             htmlFor="book-book-author"
             placeholder="book authour"
-            className="input two"
+            className="input"
             {...register('author', { required: true, placeholder: 'book author', minLength: 2 })}
           />
         </label>
@@ -54,6 +53,24 @@ const BookForm = () => {
             <option>Self-Help</option>
             <option>financial</option>
           </select>
+        </label>
+        <label htmlFor="total-chapters">
+          <input
+            type="number"
+            htmlFor="total-chaper"
+            placeholder="Enter total chapters"
+            className="input"
+            {...register('total_chapters', { required: true, placeholder: 'book author', minLength: 2 })}
+          />
+        </label>
+        <label htmlFor="current_chapter">
+          <input
+            type="number"
+            htmlFor="current_chapter"
+            placeholder="Enter current chapter"
+            className="input"
+            {...register('current_chapter', { required: true, placeholder: 'book author', minLength: 2 })}
+          />
         </label>
         <button type="submit" onClick={handleSubmit}>Add Book</button>
       </div>
