e76a4e88633f122dec27091b9dec490ec66a45c1
daudi13 <davouma13@gmail.com>
2022-08-20 21:40:14 +0300

    remmove persistance redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,32 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
-import {
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import userReducer from './user/userSlice';
 import booksReducer from './books/bookSlice';
 
-const persistConfig = {
-  key: 'counter',
-  storage,
-};
-
-const reducers = combineReducers({ user: userReducer, book: booksReducer });
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-
-export default configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    books: booksReducer,
+  },
 });
+
+export default store;
