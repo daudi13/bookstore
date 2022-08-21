c7651dde8512bcd39df30966cb18721405be8b38
daudi13 <davouma13@gmail.com>
2022-08-21 17:13:49 +0300

    add loggedIn prop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => (
-  <>
+const Navbar = ({ logged }) => {
+  console.log(logged)
+  return (
+    <>
     <div className="navBar">
       <nav>
         <div className="logo">BookStore CMS</div>
-        <ul className="nav">
-          <li><NavLink className="link-btn" to="/">Home</NavLink></li>
-          <li><NavLink className="link-btn" to="Categories">Category</NavLink></li>
-        </ul>
+          {
+            logged && 
+          <ul className="nav">
+            <li><NavLink className="link-btn" to="/">Home</NavLink></li>
+            <li><NavLink className="link-btn" to="Categories">Category</NavLink></li>
+          </ul>
+        }
         <img src="./images/login.png" alt="user icon" className="userIcon" />
       </nav>
     </div>
   </>
-);
+  )
+};
 
 export default Navbar;
