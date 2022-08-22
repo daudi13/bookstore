ee73c42a6ec02bad113a9daea4d394614b2a08df
daudi13 <davouma13@gmail.com>
2022-08-22 07:54:24 +0300

    add container to accomodate navbar component

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -30,42 +30,42 @@ const LoginPage = () => {
   return (
     <>
       <Navbar logged={userState} />
-    <div className="auth-container">
-      <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
-        <p className={errMsg ? 'errMsg' : 'offscreen'} aria-live="assertive">
-          {errMsg}
-        </p>
-        <h3>Sign in</h3>
-        <div className="group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            {...register('email', { required: true, maxLength: 50 })}
-          />
-        </div>
-        <div className="group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            {...register('password', { required: true, minLength: 6 })}
-          />
-        </div>
-        <div className="submit button">
-          <button type="submit" className="btn btn-primary">
-            submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          create an account?
-          {' '}
-          <Link to={{ pathname: 'sign-up' }}>Sign-up</Link>
-        </p>
-      </form>
-    </div>
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+          <p className={errMsg ? 'errMsg' : 'offscreen'} aria-live="assertive">
+            {errMsg}
+          </p>
+          <h3>Sign in</h3>
+          <div className="group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              {...register('email', { required: true, maxLength: 50 })}
+            />
+          </div>
+          <div className="group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              {...register('password', { required: true, minLength: 6 })}
+            />
+          </div>
+          <div className="submit button">
+            <button type="submit" className="btn btn-primary">
+              submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            create an account?
+            {' '}
+            <Link to={{ pathname: 'sign-up' }}>Sign-up</Link>
+          </p>
+        </form>
+      </div>
     </>
   );
 };
