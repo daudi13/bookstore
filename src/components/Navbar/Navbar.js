a8317169afde5b38b02a8994eaaf8615ba7af4bc
daudi13 <davouma13@gmail.com>
2022-08-22 07:52:43 +0300

    add prop to Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,24 +3,26 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = ({ logged }) => {
-  console.log(logged)
+  console.log(logged);
   return (
     <>
-    <div className="navBar">
-      <nav>
-        <div className="logo">BookStore CMS</div>
+      <div className="navBar">
+        <nav>
+          <div className="logo">BookStore CMS</div>
           {
-            logged && 
+            logged
+          && (
           <ul className="nav">
             <li><NavLink className="link-btn" to="/">Home</NavLink></li>
             <li><NavLink className="link-btn" to="Categories">Category</NavLink></li>
           </ul>
+          )
         }
-        <img src="./images/login.png" alt="user icon" className="userIcon" />
-      </nav>
-    </div>
-  </>
-  )
+          <img src="./images/login.png" alt="user icon" className="userIcon" />
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
