52c86c31cfaf9be1ef9f82a2c856ef254166e05d
daudi13 <davouma13@gmail.com>
2022-08-22 08:35:20 +0300

    add overlay

--- src/components/Home/BookForm.js
+++ src/components/Home/BookForm.js
@@ -31,71 +31,69 @@ const BookForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form">
-      <h3>Add New Books</h3>
-      <div className="form-container">
-        <label htmlFor="book-title">
-          <input
-            type="text"
-            htmlFor="book-title"
-            placeholder="book title"
-            className="input"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            required
-          />
-        </label>
-        <label htmlFor="book-author">
-          <input
-            type="text"
-            htmlFor="book-book-author"
-            placeholder="book authour"
-            className="input"
-            onChange={(e) => setAuthor(e.target.value)}
-            value={author}
-            required
-          />
-        </label>
-        <label htmlFor="genre">
-          <select
-            id="books genre"
-            className="category"
-            onChange={(e) => setGenre(e.target.value)}
-            value={genre}
-            required
-          >
-            <option>genre</option>
-            <option>Fiction</option>
-            <option>Non-fiction</option>
-            <option>Self-Help</option>
-            <option>financial</option>
-          </select>
-        </label>
-        <label htmlFor="total-chapters">
-          <input
-            type="number"
-            htmlFor="total-chaper"
-            placeholder="Enter total chapters"
-            className="input"
-            value={totalChapters}
-            onChange={(e) => setTotalChapters(e.target.value)}
-            required
-          />
-        </label>
-        <label htmlFor="current_chapter">
-          <input
-            type="number"
-            htmlFor="current_chapter"
-            placeholder="Enter current chapter"
-            className="input"
-            value={currentChapter}
-            onChange={(e) => setCurrentChapter(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit" onClick={handleSubmit}>Add Book</button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="form">
+        <h3>Add New Books</h3>
+        <div className="form-container">
+          <label htmlFor="book-title">
+            <input
+              type="text"
+              htmlFor="book-title"
+              placeholder="book title"
+              className="input"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required
+            />
+          </label>
+          <label htmlFor="book-author">
+            <input
+              type="text"
+              htmlFor="book-book-author"
+              placeholder="book authour"
+              className="input"
+              onChange={(e) => setAuthor(e.target.value)}
+              value={author}
+              required
+            />
+          </label>
+          <label htmlFor="genre">
+            <input
+              id="books genre"
+              placeholder="genre"
+              className="input"
+              onChange={(e) => setGenre(e.target.value)}
+              value={genre}
+              required
+            />
+          </label>
+          <label htmlFor="total-chapters">
+            <input
+              type="number"
+              htmlFor="total-chaper"
+              placeholder="Enter total chapters"
+              className="input"
+              value={totalChapters}
+              onChange={(e) => setTotalChapters(e.target.value)}
+              required
+            />
+          </label>
+          <label htmlFor="current_chapter">
+            <input
+              type="number"
+              htmlFor="current_chapter"
+              placeholder="Enter current chapter"
+              className="input"
+              value={currentChapter}
+              onChange={(e) => setCurrentChapter(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit" onClick={handleSubmit}>Add Book</button>
+        </div>
+      </form>
+      <div className="overlay" />
+    </>
   );
 };
 
