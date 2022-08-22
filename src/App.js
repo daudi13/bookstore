ccc627a450fe99532fd145b39891409c970f4e4b
daudi13 <davouma13@gmail.com>
2022-08-22 12:42:12 +0300

    add Navbar to App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import BookContainer from './components/Home/BookContainer';
-import { useSelector } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 
-const App = () => {
-  return (
-    <>
-      <Navbar logged={true}/>
-      <BookContainer />
-    </>
-  )
-}
+const App = () => (
+  <>
+    <Navbar logged />
+    <BookContainer />
+  </>
+);
 
 export default App;
