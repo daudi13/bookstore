487278bd0a4c0f17f09f79ab90b4e36680e920a1
daudi13 <davouma13@gmail.com>
2022-08-30 06:54:51 +0300

    add login success notification

--- src/components/Home/BookContainer.js
+++ src/components/Home/BookContainer.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BookForm from './BookForm';
 import './BookContainer.css';
 import BookList from './BookList';
 
 const BookContainer = () => {
   const [bookform, setBookForm] = useState(false);
+  const [alert, setAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2500);
+  }, [alert]);
+
   return (
     <div className="row">
+      {
+        alert && <p className="success">you&apos;re logged in!!</p>
+      }
       <BookList />
       <button type="button" onClick={() => setBookForm(true)} className="addBook" title="Add Book" aria-label="add book">+</button>
       {
